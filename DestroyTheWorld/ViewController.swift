b2fa1341c52a915fee33e47c02fcb9ecb98fbe72
mhwgo <mhwgo@node-1w7jr9qm1ual1jqtxipwskjf0.ipv6.telus.net>
2017-11-01 23:52:33 -0700

    Initial Commit

---
+++ DestroyTheWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DestroyTheWorld
+//
+//  Created by mhwgo on 2017-11-01.
+//  Copyright © 2017 mhwgo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
